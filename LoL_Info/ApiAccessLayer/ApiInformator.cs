e56d460e42a3570a821bb609a76697144a64075d
Dima1247 <palamarchuk1247@gmail.com>
2018-08-10 19:07:48 +0300

    Add files via upload

---
+++ LoL_Info/ApiAccessLayer/ApiInformator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ApiAccessLayer
+{
+    class ApiInformator<DataType>
+    {
+        private HttpWebRequest request;
+        private HttpWebResponse response;
+        private StreamReader reader;
+
+        private string path, jsonInfo;
+
+        public ApiInformator(string apiPath)
+        {
+            path = apiPath;
+        }
+
+        public DataType GetInfoInType()
+        {
+            MakeRequest();
+            GetResponse();
+            ReadResponse();
+
+            return JsonConvert.DeserializeObject<DataType>(jsonInfo);
+        }
+
+        private void MakeRequest()
+        {
+            request = (HttpWebRequest)WebRequest.Create(path);
+        }
+
+        private void GetResponse()
+        {
+            response = (HttpWebResponse)request.GetResponse();
+        }
+
+        private void ReadResponse()
+        {
+            reader = new StreamReader(response.GetResponseStream());
+            jsonInfo = reader.ReadToEnd();
+        }
+        
+        ~ApiInformator()
+        {
+            try
+            {
+                reader.Close();
+                response.Close();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ LoL_Info/ApiAccessLayer/Finder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiAccessLayer
+{
+    public class Finder<Type>
+    {
+        public Finder(ref Type finalInfo, string path)
+        {
+            finalInfo = new ApiInformator<Type>(path).GetInfoInType();
+        }
+    }
+}
